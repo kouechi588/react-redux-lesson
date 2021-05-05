e231e30ecd79ca560a2f44b29ebab6e72cc3297a
kouechi588 <kouechi588@gmail.com>
2021-05-05 19:31:05 +0900

    learn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function App() {
+function App2() {
   return (
-		<h1>
-			Hello World!!
-		</h1>
+		<>
+			<label htmlFor="foo">foo</label>
+			<input id="foo" type="text" onChange={() => {console.log("changed")}}/>
+			<h1>Hello World!!</h1>
+		</>
   );
+}
+
+const App = () => {
+	return (
+		<div>
+			<Cat></Cat>
+			<Cat></Cat>
+		</div>
+	)
+}
+
+const Cat = () => {
+	return <div>Meow!</div>
 }
 
 export default App;
