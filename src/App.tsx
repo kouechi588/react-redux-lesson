5bdd933d858fd148be372b0d5af72dce640b2863
kouechi588 <kouechi588@gmail.com>
2021-05-05 21:40:52 +0900

    learn state

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-	const profiles: UserType[] = [
-		{name: "taro", age: 30},
-		{name: "kei", age: 28},
-	]
 
+const App = () => {
 	return (
 		<div>
-			{
-			profiles.map((profile, index) => {
-				return <User name={profile.name} age={profile.age} key={index}/>
-			})
-			}
+			<Counter count={100}></Counter>
 		</div>
 	)
 }
 
-type UserType = {
-	name: string,
-	age: number
+type Props = {
+	count: number
 }
 
-const User = (props: UserType) => {
-	return <div>name is {props.name}, age is {props.age}</div>
+type State = {
+	count: number
 }
+
+class Counter extends Component<Props, State> {
+	//コンポーネント初期化時にコールされる
+	constructor(props: Props){
+		super(props)
+		this.state = { count: 0 }
+		console.log(this.state)
+	}
+
+	plus = () => {
+		this.setState({ count: this.state.count + 1 })
+	}
+
+	minus = () => {
+		//状態に関連するDOMを再レンダーする, 直接stateは更新NG
+		//setStateはrenderを実行する
+		this.setState({ count: this.state.count - 1 })
+	}
+	
+	render() {
+		return (
+			<>
+				<div>counter: { this.state.count }</div>
+				<button onClick={this.plus}>plus</button>
+				<button onClick={this.minus}>minus</button>
+			</>
+		)
+	}
+}
+
+// state: コンポーネント内部でのみ利用可能
+// state: mutable
+
+// type UserType = {
+// 	name: string,
+// 	age: number
+// }
+
+// const User = (props: UserType) => {
+// 	return <div>name is {props.name}, age is {props.age}</div>
+// }
 
 export default App;
